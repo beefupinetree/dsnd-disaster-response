09f78fd9a72c99e21178883cfd921538237de1e8
Tarek Samhan <tsamhan@gmail.com>
2018-12-25 15:07:33 -0500

    fixed graphs

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -76,7 +76,7 @@ def build_model():
     parameters = {'clf__estimator__n_estimators': [10, 20],
                   'clf__estimator__min_samples_split': [2, 4]
                   }
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2)
     return cv
 
 
